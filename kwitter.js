485a60b5a04b1f46a79c82fd2bba8e774c591017
FandomLover <87060974+FandomLover@users.noreply.github.com>
2021-07-13 15:12:31 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,13 @@
+localStorage.getItem("input")
+var firebaseConfig = {
+    apiKey: "AIzaSyB_-RqqVyL_VtX8OQuRGCG-P1AL32jQ9ts",
+    authDomain: "kwitter-71772.firebaseapp.com",
+    databaseURL: "https://kwitter-71772-default-rtdb.firebaseio.com",
+    projectId: "kwitter-71772",
+    storageBucket: "kwitter-71772.appspot.com",
+    messagingSenderId: "954306763397",
+    appId: "1:954306763397:web:39bf22c8c1b2e0d65b67ec",
+    measurementId: "G-7C3S7HMN4B"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
